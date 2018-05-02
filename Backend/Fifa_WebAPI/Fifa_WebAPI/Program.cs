1f88dc2525640f776b5d41315e6b493ac600c803
GLoGitH <Gert.Longin@ventigrate.be>
2018-05-02 16:47:48 +0200

    added using models

--- Backend/Fifa_WebAPI/Fifa_WebAPI/Program.cs
+++ Backend/Fifa_WebAPI/Fifa_WebAPI/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Fifa_WebAPI.DbContext;
+using Fifa_WebAPI.Models;
 
 
 namespace Fifa_WebAPI
