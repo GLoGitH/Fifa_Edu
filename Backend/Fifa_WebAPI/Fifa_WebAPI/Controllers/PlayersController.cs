c07cacd472cc56ec7b6fb834ebf4901ec2e8697b
GLoGitH <Gert.Longin@ventigrate.be>
2018-05-04 17:01:11 +0200

    added controllers to WebAPI backend

---
+++ Backend/Fifa_WebAPI/Fifa_WebAPI/Controllers/PlayersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fifa_WebAPI.DbContext;
+using Fifa_WebAPI.Models;
+
+namespace Fifa_WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Players")]
+    public class PlayersController : Controller
+    {
+        private readonly FifaDbContext _context;
+
+        public PlayersController(FifaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Players
+        [HttpGet]
+        public IEnumerable<Player> GetPlayers()
+        {
+            return _context.Players;
+        }
+
+        // GET: api/Players/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPlayer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var player = await _context.Players.SingleOrDefaultAsync(m => m.PlayerID == id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(player);
+        }
+
+        // PUT: api/Players/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayer([FromRoute] int id, [FromBody] Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != player.PlayerID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Players
+        [HttpPost]
+        public async Task<IActionResult> PostPlayer([FromBody] Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Players.Add(player);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayer", new { id = player.PlayerID }, player);
+        }
+
+        // DELETE: api/Players/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var player = await _context.Players.SingleOrDefaultAsync(m => m.PlayerID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _context.Players.Remove(player);
+            await _context.SaveChangesAsync();
+
+            return Ok(player);
+        }
+
+        private bool PlayerExists(int id)
+        {
+            return _context.Players.Any(e => e.PlayerID == id);
+        }
+    }
+}
